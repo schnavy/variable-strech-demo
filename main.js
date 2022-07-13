92d09afd8969d98f4b5fe5a50c91e921a2f900e2
David <50714711+schnavy@users.noreply.github.com>
2022-07-13 16:37:05 +0200

    safari ios bug fix

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ window.addEventListener('resize', updateFontStrech)
 function updateFontStrech () {
   let val = map_range(window.innerWidth, MIN_TEXT_WIDTH, MAX_TEXT_WIDTH, 1.5, 8)
   setFontStrech(val)
-
+  let loopCount = 0
   if (varTitle.clientWidth && window.innerWidth) {
-    while (varTitle.clientWidth > window.innerWidth) {
+    while (varTitle.clientWidth > window.innerWidth && loopCount < 100) {
       console.log('Alarm')
       val -= 0.1
       varTitle.style.fontVariationSettings = '"wdth" ' + val
+      loopCount++
     }
   }
 }
@@ -25,5 +26,8 @@ function setFontStrech (val) {
 }
 
 function map_range (value, low1, high1, low2, high2) {
-  return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
+  let res = low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
+  res = Math.min(res, low2)
+  res = Math.max(res, high2)
+  return res
 }
