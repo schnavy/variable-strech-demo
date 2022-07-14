501f0c15ab66128fd5df0e8f02ac28a825b175a2
David <50714711+schnavy@users.noreply.github.com>
2022-07-14 17:58:01 +0200

    vertical scaing

--- main.js
+++ main.js
@@ -2,19 +2,36 @@ let MIN_TEXT_WIDTH = 400
 let MAX_TEXT_WIDTH = 1600
 
 let varTitle = document.querySelector('#variable-title')
+let body = document.querySelector('body')
 
 window.addEventListener('load', updateFontStrech)
 
 window.addEventListener('resize', updateFontStrech)
+
+
+let scrollPosition = document.scrollingElement.scrollTop
+
+document.addEventListener('scroll', e => {
+  scrollPosition = document.scrollingElement.scrollTop
+
+  varTitle.style.transform =
+    'translateY(-' +
+    (10 + scrollPosition / 50) +
+    'px) scaleY(' +
+    (1 + scrollPosition / 1000) +
+    ')'
+})
+
+
 
 function updateFontStrech () {
   let val = map_range(window.innerWidth, MIN_TEXT_WIDTH, MAX_TEXT_WIDTH, 1.5, 8)
   setFontStrech(val)
   let loopCount = 0
   if (varTitle.clientWidth && window.innerWidth) {
-    while (varTitle.clientWidth > window.innerWidth && loopCount < 100) {
+    while (varTitle.clientWidth > window.innerWidth && loopCount < 1000) {
       console.log('Alarm')
-      val -= 0.1
+      val -= 0.05
       varTitle.style.fontVariationSettings = '"wdth" ' + val
       loopCount++
     }
@@ -31,3 +48,4 @@ function map_range (value, low1, high1, low2, high2) {
   res = Math.max(res, high2)
   return res
 }
+
