10073ad3c58b5b0c2bd6c50550a403098c9bafc2
David <50714711+schnavy@users.noreply.github.com>
2022-07-15 18:21:51 +0200

    drei optionen

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 let MIN_TEXT_WIDTH = 400
 let MAX_TEXT_WIDTH = 1600
 
-let verticalStrechBool = true
+const BASE_FONT_SIZE = 95
+
+let verticalStretchBool = false
+let verticalVariableStretchBool = true
 
 let varTitle = document.querySelector('#variable-title')
 let body = document.querySelector('body')
 
-window.addEventListener('load', updateFontStrech)
+window.addEventListener('load', updateFontStretch)
 
-window.addEventListener('resize', updateFontStrech)
+window.addEventListener('resize', updateFontStretch)
 
 let scrollPosition = document.scrollingElement.scrollTop
 
-document.addEventListener('scroll', updateVerticalStrech)
+document.addEventListener('scroll', scrollStretchHandler)
 
+function scrollStretchHandler() {
+  if(verticalStretchBool){
+    updateVerticalStretch()
+  }else if(verticalVariableStretchBool){
+    updateVerticalStretchVariable()
+  }
+}
 
-function updateFontStrech () {
+function updateFontStretch () {
   let val = map_range(window.innerWidth, MIN_TEXT_WIDTH, MAX_TEXT_WIDTH, 1.5, 8)
-  setFontStrech(val)
+  setFontStretch(val)
   let loopCount = 0
   if (varTitle.clientWidth && window.innerWidth) {
-    while (varTitle.clientWidth > window.innerWidth-20 && loopCount < 1000) {
-      console.log('Alarm')
+    while (varTitle.clientWidth > window.innerWidth - 20 && loopCount < 1000) {
       val -= 0.05
       varTitle.style.fontVariationSettings = '"wdth" ' + val
       loopCount++
@@ -29,7 +38,7 @@ function updateFontStrech () {
   }
 }
 
-function setFontStrech (val) {
+function setFontStretch (val) {
   varTitle.style.fontVariationSettings = '"wdth" ' + val
 }
 
@@ -37,37 +46,56 @@ function map_range (value, low1, high1, low2, high2) {
   let res = low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
   res = Math.max(res, low2)
   res = Math.min(res, high2)
-  console.log(res);
   return res
 }
 
-function toggleVerticalStrech (elem) {
-  if (elem.checked) {
-    verticalStrechBool = true
-  } else {
-    verticalStrechBool = false
+function toggleVerticalStretch (elem) {
+  if (elem.value == "verticalStretchBool") {
+    verticalStretchBool = true
+    verticalVariableStretchBool = false
+  } else if(elem.value == "verticalVariableStretchBool") {
+    verticalStretchBool = false
+    verticalVariableStretchBool = true
+  }else{
+    verticalStretchBool = false
+    verticalVariableStretchBool = false
   }
-  updateVerticalStrech()
+  scrollStretchHandler()
+  updateVerticalStretch()
+  updateVerticalStretchVariable()
+
 }
 
-
-function updateVerticalStrech () {
-  if (verticalStrechBool) {
+function updateVerticalStretch () {
+  if (verticalStretchBool) {
     scrollPosition = document.scrollingElement.scrollTop
 
-    varTitle.style.transform =
-      'scaleY(' +
-      (1 + scrollPosition / 1000) +
-      ')'
+    varTitle.style.transform = 'scaleY(' + (1 + scrollPosition / 1000) + ')'
 
-
-      // varTitle.style.transform =
-      // 'translateY(-' +
-      // (0 + scrollPosition / 50) +
-      // 'px) scaleY(' +
-      // (1 + scrollPosition / 1000) +
-      // ')'
+    // varTitle.style.transform =
+    // 'translateY(-' +
+    // (0 + scrollPosition / 50) +
+    // 'px) scaleY(' +
+    // (1 + scrollPosition / 1000) +
+    // ')'
   } else {
     varTitle.style.transform = 'translateY(-0px) scaleY(1)'
+  }
+}
+
+function updateVerticalStretchVariable () {
+
+  
+  if (verticalVariableStretchBool) {
+    scrollPosition = document.scrollingElement.scrollTop
+  
+    console.log(scrollPosition)
+    let newVal = BASE_FONT_SIZE + scrollPosition / 10
+
+    varTitle.style.fontSize = newVal + 'px'
+    updateFontStretch()
+  } else {
+    varTitle.style.fontSize = BASE_FONT_SIZE + 'px'
+    updateFontStretch()
   }
 }
