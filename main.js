9b5cd5d6f7aaeeb7fc0bb9867e804d144c3f36d3
David <50714711+schnavy@users.noreply.github.com>
2022-07-15 09:07:26 +0200

    strech inlk button styling

--- main.js
+++ main.js
@@ -14,12 +14,13 @@ let scrollPosition = document.scrollingElement.scrollTop
 
 document.addEventListener('scroll', updateVerticalStrech)
 
+
 function updateFontStrech () {
   let val = map_range(window.innerWidth, MIN_TEXT_WIDTH, MAX_TEXT_WIDTH, 1.5, 8)
   setFontStrech(val)
   let loopCount = 0
   if (varTitle.clientWidth && window.innerWidth) {
-    while (varTitle.clientWidth > window.innerWidth && loopCount < 1000) {
+    while (varTitle.clientWidth > window.innerWidth-20 && loopCount < 1000) {
       console.log('Alarm')
       val -= 0.05
       varTitle.style.fontVariationSettings = '"wdth" ' + val
@@ -34,8 +35,9 @@ function setFontStrech (val) {
 
 function map_range (value, low1, high1, low2, high2) {
   let res = low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
-  res = Math.min(res, low2)
-  res = Math.max(res, high2)
+  res = Math.max(res, low2)
+  res = Math.min(res, high2)
+  console.log(res);
   return res
 }
 
@@ -48,17 +50,24 @@ function toggleVerticalStrech (elem) {
   updateVerticalStrech()
 }
 
+
 function updateVerticalStrech () {
   if (verticalStrechBool) {
     scrollPosition = document.scrollingElement.scrollTop
 
     varTitle.style.transform =
-      'translateY(-' +
-      (10 + scrollPosition / 50) +
-      'px) scaleY(' +
+      'scaleY(' +
       (1 + scrollPosition / 1000) +
       ')'
+
+
+      // varTitle.style.transform =
+      // 'translateY(-' +
+      // (0 + scrollPosition / 50) +
+      // 'px) scaleY(' +
+      // (1 + scrollPosition / 1000) +
+      // ')'
   } else {
-    varTitle.style.transform = 'translateY(-10px) scaleY(1)'
+    varTitle.style.transform = 'translateY(-0px) scaleY(1)'
   }
 }
